93cd5580196bd2c6e42db1ffec52414f0392d85e
Daniel Trone <royhobbstn@yahoo.com>
2016-03-15 21:54:57 +0000

    add port because dola database is on fast ssd drive which is port 5433

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 var pg = require('pg');
-var conString = "postgres://codemog:demography@104.197.26.248/dola";
+var conString = "postgres://codemog:demography@104.197.26.248:5433/dola";
 
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
