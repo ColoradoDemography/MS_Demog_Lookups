4dd41c2be405817e19f6b2f8669acd7a448b55b3
TBleess <todd.bleess@gmail.com>
2020-04-25 12:40:50 -0600

    Update jobs_by_sector.js

--- routes/jobs_by_sector.js
+++ routes/jobs_by_sector.js
@@ -209,7 +209,7 @@ app.get('/jobs0', function(req, res) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"];
+        var yeardomain = ["2018"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
