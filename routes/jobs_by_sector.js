c10586d4d6eb26148b41ebe87cd2d9c90398c5bc
TBleess <todd.bleess@gmail.com>
2020-08-28 11:30:42 -0600

    2019

--- routes/jobs_by_sector.js
+++ routes/jobs_by_sector.js
@@ -68,7 +68,7 @@ module.exports = function(app, pg, conString) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"];
+        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
@@ -390,7 +390,7 @@ app.get('/jobs0', function(req, res) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018"];
+        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
