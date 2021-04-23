eb4a6790cde4df990cc951bd87333869310bb28c
TBleess <todd.bleess@state.co.us>
2021-04-23 12:39:53 -0600

    FFS

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ require('./routes/county_muni_timeseries.js')(app, pg, conString);
 require('./routes/historical_census.js')(app, pg, conString);
 require('./routes/jobs_forecast.js')(app, pg, conString);
 require('./routes/wage_data.js')(app, pg, conString);
-require('./routes/netmig.js')(app, pg, conString);
+//require('./routes/netmig.js')(app, pg, conString);
 
 
 var server = app.listen(4001, function() {
