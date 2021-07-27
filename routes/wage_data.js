ce80dee8fb57e8a3ef1164e92ccaaf398f3a9e0c
TBleess <todd.bleess@state.co.us>
2021-07-27 14:05:48 -0600

    2020

--- routes/wage_data.js
+++ routes/wage_data.js
@@ -56,7 +56,7 @@ module.exports = function(app, pg, conString) {
         if (!req.query.year) {
             res.send('please specify a year (or comma separated list of years)');
         } else {
-            yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
+            yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
             //create array of years
             year = (req.query.year).split(",");
             //validate year input
@@ -210,7 +210,7 @@ module.exports = function(app, pg, conString) {
         if (!req.query.year) {
             res.send('please specify a year (or comma separated list of years)');
         } else {
-            yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
+            yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
             //create array of years
             year = (req.query.year).split(",");
             //validate year input
