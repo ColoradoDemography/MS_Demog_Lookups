7a63e05fa60a603ef8529b3b6cfee0557c1bc8c6
TBleess <todd.bleess@state.co.us>
2021-07-28 08:15:55 -0600

    Missed a year

--- routes/jobs_by_sector.js
+++ routes/jobs_by_sector.js
@@ -68,7 +68,7 @@ module.exports = function(app, pg, conString) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019"];
+        var yeardomain = ["2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
