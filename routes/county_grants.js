f5dfbe547ec934071236cfcd73ee50b3692a0766
TBleess <todd.bleess@state.co.us>
2023-01-13 14:21:56 -0700

    Update county_grants.js

--- routes/county_grants.js
+++ routes/county_grants.js
@@ -1,8 +1,370 @@
+//ACS geojson has the csv, not sure why
+//module.exports = function(app, pg, csv, conString){
 module.exports = function(app, pg, conString) {
 
 
-    app.get('/county_grants', function(req, res) {
+    app.get('/geojson', function(req, res) {
 
+  var lastbranchdone=0;
+
+//PHP.js
+function strpos(haystack, needle, offset) {
+  //  discuss at: http://phpjs.org/functions/strpos/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  // bugfixed by: Daniel Esteban
+  //   example 1: strpos('Kevin van Zonneveld', 'e', 5);
+  //   returns 1: 14
+
+  var i = (haystack + '')
+    .indexOf(needle, (offset || 0));
+  return i === -1 ? false : i;
+}
+
+function str_replace(search, replace, subject, count) {
+  //  discuss at: http://phpjs.org/functions/str_replace/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Gabriel Paderni
+  // improved by: Philip Peterson
+  // improved by: Simon Willison (http://simonwillison.net)
+  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //  revised by: Jonas Raoni Soares Silva (http://www.jsfromhell.com)
+  // bugfixed by: Anton Ongson
+  // bugfixed by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // bugfixed by: Oleg Eremeev
+  //    input by: Onno Marsman
+  //    input by: Brett Zamir (http://brett-zamir.me)
+  //    input by: Oleg Eremeev
+  //        note: The count parameter must be passed as a string in order
+  //        note: to find a global variable in which the result will be given
+  //   example 1: str_replace(' ', '.', 'Kevin van Zonneveld');
+  //   returns 1: 'Kevin.van.Zonneveld'
+  //   example 2: str_replace(['{name}', 'l'], ['hello', 'm'], '{name}, lars');
+  //   returns 2: 'hemmo, mars'
+
+  var i = 0,
+    j = 0,
+    temp = '',
+    repl = '',
+    sl = 0,
+    fl = 0,
+    f = [].concat(search),
+    r = [].concat(replace),
+    s = subject,
+    ra = Object.prototype.toString.call(r) === '[object Array]',
+    sa = Object.prototype.toString.call(s) === '[object Array]';
+  s = [].concat(s);
+  if (count) {
+    this.window[count] = 0;
+  }
+
+  for (i = 0, sl = s.length; i < sl; i++) {
+    if (s[i] === '') {
+      continue;
+    }
+    for (j = 0, fl = f.length; j < fl; j++) {
+      temp = s[i] + '';
+      repl = ra ? (r[j] !== undefined ? r[j] : '') : r[0];
+      s[i] = (temp)
+        .split(f[j])
+        .join(repl);
+      if (count && s[i] !== temp) {
+        this.window[count] += (temp.length - s[i].length) / f[j].length;
+      }
+    }
+  }
+  return sa ? s : s[0];
+}  
+  
+function substr_replace(str, replace, start, length) {
+  //  discuss at: http://phpjs.org/functions/substr_replace/
+  // original by: Brett Zamir (http://brett-zamir.me)
+
+  if (start < 0) { // start position in str
+    start = start + str.length;
+  }
+  length = length !== undefined ? length : str.length;
+  if (length < 0) {
+    length = length + str.length - start;
+  }
+
+  return str.slice(0, start) + replace.substr(0, length) + replace.slice(length) + str.slice(start + length);
+}  
+
+function explode(delimiter, string, limit) {
+  //  discuss at: http://phpjs.org/functions/explode/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //   example 1: explode(' ', 'Kevin van Zonneveld');
+  //   returns 1: {0: 'Kevin', 1: 'van', 2: 'Zonneveld'}
+
+  if (arguments.length < 2 || typeof delimiter === 'undefined' || typeof string === 'undefined') return null;
+  if (delimiter === '' || delimiter === false || delimiter === null) return false;
+  if (typeof delimiter === 'function' || typeof delimiter === 'object' || typeof string === 'function' || typeof string ===
+    'object') {
+    return {
+      0: ''
+    };
+  }
+  if (delimiter === true) delimiter = '1';
+
+  // Here we go...
+  delimiter += '';
+  string += '';
+
+  var s = string.split(delimiter);
+
+  if (typeof limit === 'undefined') return s;
+
+  // Support for limit
+  if (limit === 0) limit = 1;
+
+  // Positive limit
+  if (limit > 0) {
+    if (limit >= s.length) return s;
+    return s.slice(0, limit - 1)
+      .concat([s.slice(limit - 1)
+        .join(delimiter)
+      ]);
+  }
+
+  // Negative limit
+  if (-limit >= s.length) return [];
+
+  s.splice(s.length + limit);
+  return s;
+}
+  
+function substr(str, start, len) {
+  //  discuss at: http://phpjs.org/functions/substr/
+  //     version: 909.322
+  // original by: Martijn Wieringa
+  // bugfixed by: T.Wild
+  // improved by: Onno Marsman
+  // improved by: Brett Zamir (http://brett-zamir.me)
+  //  revised by: Theriault
+  //        note: Handles rare Unicode characters if 'unicode.semantics' ini (PHP6) is set to 'on'
+  //   example 1: substr('abcdef', 0, -1);
+  //   returns 1: 'abcde'
+  //   example 2: substr(2, 0, -6);
+  //   returns 2: false
+  //   example 3: ini_set('unicode.semantics',  'on');
+  //   example 3: substr('a\uD801\uDC00', 0, -1);
+  //   returns 3: 'a'
+  //   example 4: ini_set('unicode.semantics',  'on');
+  //   example 4: substr('a\uD801\uDC00', 0, 2);
+  //   returns 4: 'a\uD801\uDC00'
+  //   example 5: ini_set('unicode.semantics',  'on');
+  //   example 5: substr('a\uD801\uDC00', -1, 1);
+  //   returns 5: '\uD801\uDC00'
+  //   example 6: ini_set('unicode.semantics',  'on');
+  //   example 6: substr('a\uD801\uDC00z\uD801\uDC00', -3, 2);
+  //   returns 6: '\uD801\uDC00z'
+  //   example 7: ini_set('unicode.semantics',  'on');
+  //   example 7: substr('a\uD801\uDC00z\uD801\uDC00', -3, -1)
+  //   returns 7: '\uD801\uDC00z'
+
+  var i = 0,
+    allBMP = true,
+    es = 0,
+    el = 0,
+    se = 0,
+    ret = '';
+  str += '';
+  var end = str.length;
+
+  // BEGIN REDUNDANT
+  this.php_js = this.php_js || {};
+  this.php_js.ini = this.php_js.ini || {};
+  // END REDUNDANT
+  switch ((this.php_js.ini['unicode.semantics'] && this.php_js.ini['unicode.semantics'].local_value.toLowerCase())) {
+    case 'on':
+      // Full-blown Unicode including non-Basic-Multilingual-Plane characters
+      // strlen()
+      for (i = 0; i < str.length; i++) {
+        if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+          allBMP = false;
+          break;
+        }
+      }
+
+      if (!allBMP) {
+        if (start < 0) {
+          for (i = end - 1, es = (start += end); i >= es; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              start--;
+              es--;
+            }
+          }
+        } else {
+          var surrogatePairs = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
+          while ((surrogatePairs.exec(str)) != null) {
+            var li = surrogatePairs.lastIndex;
+            if (li - 2 < start) {
+              start++;
+            } else {
+              break;
+            }
+          }
+        }
+
+        if (start >= end || start < 0) {
+          return false;
+        }
+        if (len < 0) {
+          for (i = end - 1, el = (end += len); i >= el; i--) {
+            if (/[\uDC00-\uDFFF]/.test(str.charAt(i)) && /[\uD800-\uDBFF]/.test(str.charAt(i - 1))) {
+              end--;
+              el--;
+            }
+          }
+          if (start > end) {
+            return false;
+          }
+          return str.slice(start, end);
+        } else {
+          se = start + len;
+          for (i = start; i < se; i++) {
+            ret += str.charAt(i);
+            if (/[\uD800-\uDBFF]/.test(str.charAt(i)) && /[\uDC00-\uDFFF]/.test(str.charAt(i + 1))) {
+              se++; // Go one further, since one of the "characters" is part of a surrogate pair
+            }
+          }
+          return ret;
+        }
+        break;
+      }
+      // Fall-through
+    case 'off':
+      // assumes there are no non-BMP characters;
+      //    if there may be such characters, then it is best to turn it on (critical in true XHTML/XML)
+    default:
+      if (start < 0) {
+        start += end;
+      }
+      end = typeof len === 'undefined' ? end : (len < 0 ? len + end : len + start);
+      // PHP returns false if start does not fall within the string.
+      // PHP returns false if the calculated end comes before the calculated start.
+      // PHP returns an empty string if start and end are the same.
+      // Otherwise, PHP returns the portion of the string from start to end.
+      return start >= str.length || start < 0 || start > end ? !1 : str.slice(start, end);
+  }
+  return undefined; // Please Netbeans
+}
+  
+function implode(glue, pieces) {
+  //  discuss at: http://phpjs.org/functions/implode/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Waldo Malqui Silva
+  // improved by: Itsacon (http://www.itsacon.net/)
+  // bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //   example 1: implode(' ', ['Kevin', 'van', 'Zonneveld']);
+  //   returns 1: 'Kevin van Zonneveld'
+  //   example 2: implode(' ', {first:'Kevin', last: 'van Zonneveld'});
+  //   returns 2: 'Kevin van Zonneveld'
+
+  var i = '',
+    retVal = '',
+    tGlue = '';
+  if (arguments.length === 1) {
+    pieces = glue;
+    glue = '';
+  }
+  if (typeof pieces === 'object') {
+    if (Object.prototype.toString.call(pieces) === '[object Array]') {
+      return pieces.join(glue);
+    }
+    for (i in pieces) {
+      retVal += tGlue + pieces[i];
+      tGlue = glue;
+    }
+    return retVal;
+  }
+  return pieces;
+}  
+  
+function array_merge() {
+  //  discuss at: http://phpjs.org/functions/array_merge/
+  // original by: Brett Zamir (http://brett-zamir.me)
+  // bugfixed by: Nate
+  // bugfixed by: Brett Zamir (http://brett-zamir.me)
+  //    input by: josh
+  //   example 1: arr1 = {"color": "red", 0: 2, 1: 4}
+  //   example 1: arr2 = {0: "a", 1: "b", "color": "green", "shape": "trapezoid", 2: 4}
+  //   example 1: array_merge(arr1, arr2)
+  //   returns 1: {"color": "green", 0: 2, 1: 4, 2: "a", 3: "b", "shape": "trapezoid", 4: 4}
+  //   example 2: arr1 = []
+  //   example 2: arr2 = {1: "data"}
+  //   example 2: array_merge(arr1, arr2)
+  //   returns 2: {0: "data"}
+
+  var args = Array.prototype.slice.call(arguments),
+    argl = args.length,
+    arg,
+    retObj = {},
+    k = '',
+    argil = 0,
+    j = 0,
+    i = 0,
+    ct = 0,
+    toStr = Object.prototype.toString,
+    retArr = true;
+
+  for (i = 0; i < argl; i++) {
+    if (toStr.call(args[i]) !== '[object Array]') {
+      retArr = false;
+      break;
+    }
+  }
+
+  if (retArr) {
+    retArr = [];
+    for (i = 0; i < argl; i++) {
+      retArr = retArr.concat(args[i]);
+    }
+    return retArr;
+  }
+
+  for (i = 0, ct = 0; i < argl; i++) {
+    arg = args[i];
+    if (toStr.call(arg) === '[object Array]') {
+      for (j = 0, argil = arg.length; j < argil; j++) {
+        retObj[ct++] = arg[j];
+      }
+    } else {
+      for (k in arg) {
+        if (arg.hasOwnProperty(k)) {
+          if (parseInt(k, 10) + '' === k) {
+            retObj[ct++] = arg[k];
+          } else {
+            retObj[k] = arg[k];
+          }
+        }
+      }
+    }
+  }
+  return retObj;
+}  
+  
+function array_unshift(array) {
+  //  discuss at: http://phpjs.org/functions/array_unshift/
+  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  // improved by: Martijn Wieringa
+  // improved by: jmweb
+  //        note: Currently does not handle objects
+  //   example 1: array_unshift(['van', 'Zonneveld'], 'Kevin');
+  //   returns 1: 3
+
+  var i = arguments.length;
+
+  while (--i !== 0) {
+    arguments[0].unshift(arguments[i]);
+  }
+
+  return arguments[0].length;
+}
 
         function validate(data, check) {
             var valid;
@@ -36,7 +398,7 @@ module.exports = function(app, pg, conString) {
         //from estimates.county_sya WHERE year = 1991 or year = 1992 GROUP BY countyfips, county ORDER BY countyfips;
       
         //schema.table combination
-        var basequery = "SELECT countyfips,county,grant1,grant2,grant3,grant4 from estimates.county_grants where " + yearstring + " GROUP By countyfips, county, ORDER BY countyfips";
+        var basequery = "SELECT * from estimates.county_grants where " //+ yearstring + "and" + programstring " GROUP By countyfips, county, ORDER BY countyfips";
         var groupby = "GROUP By countyfips, county, ORDER BY countyfips";
        
 
@@ -46,7 +408,7 @@ module.exports = function(app, pg, conString) {
         
 
         var yearstring = "";
-        var countystring = "";
+        var programstring = "";
         
 
         var sqlstring;
@@ -55,9 +417,9 @@ module.exports = function(app, pg, conString) {
 
 
         //exit if no county
-        if (!req.query.county) {
-            res.send('please specify a county (or comma separated list of counties)');
-        }
+        //if (!req.query.county) {
+            //res.send('please specify a county (or comma separated list of counties)');
+        //}
 
         //exit if no year
         if (!req.query.year) {
@@ -67,17 +429,17 @@ module.exports = function(app, pg, conString) {
         //create array of county fips codes
         var county = (req.query.county).split(",");
         var countydomain = ["0", "1", "3", "5", "7", "9", "11", "13", "14", "15", "17", "19", "21", "23", "25", "27", "29", "31", "33", "35", "37", "39", "41", "43", "45", "47", "49", "51", "53", "55", "57", "59", "61", "63", "65", "67", "69", "71", "73", "75", "77", "79", "81", "83", "85", "87", "89", "91", "93", "95", "97", "99", "101", "103", "105", "107", "109", "111", "113", "115", "117", "119", "121", "123", "125"];
-        if (!validate(county, countydomain)) {
-            res.send('one of your county inputs is not valid!');
-            return;
-        }
+        //if (!validate(county, countydomain)) {
+        //    res.send('one of your county inputs is not valid!');
+        //    return;
+        //}
 
         //create sql selector for years
-        for (j = 0; j < county.length; j++) {
-            countystring = countystring + schtbl + ".countyfips = " + county[j] + " OR ";
+        //for (j = 0; j < county.length; j++) {
+        //    countystring = countystring + schtbl + ".countyfips = " + county[j] + " OR ";
         }
         //remove stray OR from end of sql selector
-        countystring = countystring.substring(0, countystring.length - 3);
+        //countystring = countystring.substring(0, countystring.length - 3);
 
 
         //create array of years
@@ -98,7 +460,7 @@ module.exports = function(app, pg, conString) {
         //put it all together
         
 
-            sqlstring = basequery + "(" + countystring + ") AND " + "(" + yearstring + ") AND " + groupby + ";";
+            sqlstring = basequery + "(" + programstring + ") AND " + "(" + yearstring + ") AND " + groupby + ";";
             break;
 
             
