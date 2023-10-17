64ace3a12a8d3a192f2c82ecceab2f185fae38f8
TBleess <todd.bleess@state.co.us>
2023-10-17 13:02:12 -0600

    2022

--- routes/county_sya_race_estimates_current.js
+++ routes/county_sya_race_estimates_current.js
@@ -298,7 +298,7 @@ module.exports = function(app, pg, conString) {
 		//MOD 7/5/2022
         var year = (req.query.year).split(",");
 		//if (req.query.test) {
-			var yeardomain = ["2020", "2021"];
+			var yeardomain = ["2020", "2021", "2022"];
 		//} else {
         //var yeardomain = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
         //}
@@ -792,7 +792,7 @@ module.exports = function(app, pg, conString) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
+        var yeardomain = ["2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020", "2021", "2022"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
