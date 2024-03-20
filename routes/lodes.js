bf157c77b3e44ba088b7e76e32c95499b53fdab4
ABSOD <32849428+ABSOD@users.noreply.github.com>
2024-03-20 14:16:54 -0600

    Add files via upload

--- routes/lodes.js
+++ routes/lodes.js
@@ -17,59 +17,39 @@ var request = require('request');
 module.exports = function(app, pg, conString) {
 
     app.get('/lodes', function(req, res) {
-		
+		var geoarr = req.query.geonum;
+		  
        //building sql statement
 	   //Table selection
 	   if(req.query.geo == 'county'){
 		   if(req.query.choice == 'summary'){
               var basequery = 'SELECT * FROM data.otm_county_summary_sector ';
+			  var geostring = 'fips = ' + geoarr;
 		   }
-		   if(req.query.choice == 'place'){
-			    var basequery = 'SELECT * FROM data.otm_county_place_sector ';
+		if(req.query.choice == 'place'){
+			  var basequery = 'SELECT * FROM data.otm_county_place_sector ';
+			  var geostring = 'work_cty = ' + geoarr + ' OR home_cty = ' + geoarr;
 	   }
 	   };
       if(req.query.geo == 'place'){
 		   if(req.query.choice == 'summary'){
               var basequery = 'SELECT * FROM data.otm_place_summary_sector ';
+			  var geostring = 'fips = ' + geoarr;
 		   }
 		   if(req.query.choice == 'place'){
 			    var basequery = 'SELECT * FROM data.otm_place_place_sector ';
-		   }
-	   }
-
-  //create array of geocodes
-  
-  var geoarr = req.query.geonum
-   //Table selection
-	   if(req.query.geo == 'county'){
-		   if(req.query.choice == 'summary'){
-              var geostring = 'fips = ' + geoarr;
-		   }
-		   if(req.query.choice == 'place'){
-			    var geostring = 'work_cty = ' + geoarr + ' OR home_cty = ' + geoarr;
-	   }
-	   };
-      if(req.query.geo == 'place'){
-		   if(req.query.choice == 'summary'){
-              var geostring = 'fips = ' + geoarr;
-		   }
-		   if(req.query.choice == 'place'){
 			    var geostring = 'work_place = ' + geoarr + ' OR home_place = ' + geoarr;
 		   }
 	   }
- 
-        //create sql selector for years AFTER 2021
-		//var yearsting = ' year = 2021'
 
- 
-      //Geo and year selection
+       //Geo and year selection
 	  //Only 2021
 	  var sqlstring = basequery + ' WHERE ( ' + geostring + ');'
      //After 2021, when more data is in files
 	 // var sqlstring = basequery + ' WHERE ( ' + geostring + ') AND (' + yearstring + ');'
-        // send to database 
+     // send to database 
 
-        sendtodatabase(sqlstring);
+     sendtodatabase(sqlstring);
 
 }
 }
