0fcb90b0942ddd8217ab7b10f9c232cdeb122ae8
TBleess <todd.bleess@state.co.us>
2024-10-30 15:16:57 -0600

    Update jobs_by_sector.js

--- routes/jobs_by_sector.js
+++ routes/jobs_by_sector.js
@@ -350,7 +350,7 @@ app.get('/jobs0', function(req, res) {
 
         //create array of years
         var year = (req.query.year).split(",");
-        var yeardomain = ["2020", "2021", "2022"];
+        var yeardomain = ["2020", "2021", "2022", "2023"];
         if (!validate(year, yeardomain)) {
             res.send('one of your year inputs is not valid!');
             return;
